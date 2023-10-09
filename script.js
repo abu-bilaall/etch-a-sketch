727c6f43912680eca94dfda8aded8e9176b56e21
musa <m.abdulrofihi@gmail.com>
2023-10-09 16:35:25 +0100

    Complete default grid

---
+++ script.js
@@ -0,0 +1,26 @@
+// body
+const body = document.querySelector('body');
+
+// Grid generator button
+const btn = document.createElement('button');
+btn.textContent = 'Generate a new grid';
+body.appendChild(btn);
+
+// default container
+const container = document.createElement('div');
+container.classList.add('old-container');
+body.appendChild(container);
+
+// default grid generator
+let grid = 16 * 16;
+while (grid > 0) {
+    const div = document.createElement('div');
+    div.classList.add('grid');
+    container.append(div);
+    grid--;
+};
+
+// etchy etch
+container.addEventListener('mouseover', (event) => {
+    event.target.classList.add('hover');
+});
