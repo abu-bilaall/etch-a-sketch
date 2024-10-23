5c01ac5779777bdfedcd8787c4a11dcd46136e12
Abdulrofihi Musa <m.abdulrofihi@gmail.com>
2024-10-23 12:35:46 +0100

    Implement script for generating new grids

--- script.js
+++ script.js
@@ -25,3 +25,31 @@ while (grid > 0) {
 container.addEventListener('mouseover', (event) => {
     event.target.classList.add('hover');
 });
+
+// generating user-specified grid
+let userDimension;
+let defaultWidth = 800;
+btn.addEventListener('click', () => {
+    userDimension = +prompt("Enter a grid dimension below (Max. 100)");
+
+    if (userDimension > 100) {
+        console.log("Sorry. The maximum dimension is 100.");
+    } else {
+        grid = userDimension * userDimension;
+       
+        // remove default grid
+        while (container.firstChild) {
+            container.removeChild(container.firstChild);
+        }
+
+        // create a grid w/ user-specified dimension
+        while (grid > 0) {
+            const div = document.createElement('div');
+            div.classList.add('grid');
+            div.style.height = `${defaultWidth / userDimension}px`;
+            div.style.width = `${defaultWidth / userDimension}px`;
+            container.append(div);
+            grid--;
+        };
+    }
+});
