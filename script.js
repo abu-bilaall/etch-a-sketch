aae89690e53ed36292cef51ebe59ff6459aa615d
Abdulrofihi Musa <m.abdulrofihi@gmail.com>
2024-10-23 11:51:48 +0100

    Add styles to the generate button and fix old code

--- script.js
+++ script.js
@@ -3,12 +3,13 @@ const body = document.querySelector('body');
 
 // Grid generator button
 const btn = document.createElement('button');
+btn.classList.add('generate-button');
 btn.textContent = 'Generate a new grid';
 body.appendChild(btn);
 
 // default container
 const container = document.createElement('div');
-container.classList.add('old-container');
+container.classList.add('grid-container');
 body.appendChild(container);
 
 // default grid generator
